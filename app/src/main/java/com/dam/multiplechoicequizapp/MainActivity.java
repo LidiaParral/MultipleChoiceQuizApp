238b57b978d8ce0261b3f305c8d88acc2c0a69a2
LidiaParral <72860314+LidiaParral@users.noreply.github.com>
2021-05-07 16:15:56 +0200

    Initial commit

---
+++ app/src/main/java/com/dam/multiplechoicequizapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.dam.multiplechoicequizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button btnStart = findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startQuiz();
+            }
+        });
+    }
+
+    private void startQuiz() {
+        Intent i = new Intent(MainActivity.this, Quiz.class);
+        startActivity(i);
+    }
+}
---
+++ app/src/main/java/com/dam/multiplechoicequizapp/Quiz.java
@@ -0,0 +1,169 @@
+package com.dam.multiplechoicequizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Quiz extends AppCompatActivity {
+
+    private TextView tvQuestion;
+    private TextView tvScore;
+    private TextView tvQuestionC;
+    private TextView tvTime;
+    private RadioGroup rbGroup;
+    private RadioButton rb1;
+    private RadioButton rb2;
+    private RadioButton rb3;
+    private RadioButton rb4;
+    private Button btnConfirm;
+
+    private ColorStateList textColorDefaultRb;
+
+
+    private List<Question> questionList;
+    private int questionCounter;
+    private int questionCountTotal;
+    private Question currentQuestion;
+
+    private int score;
+    private boolean answered;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+
+        tvQuestion = findViewById(R.id.tvQuestion);
+        tvScore = findViewById(R.id.tvScore);
+        tvQuestionC = findViewById(R.id.tvQuestionC);
+        tvTime = findViewById(R.id.tvTime);
+        rbGroup = findViewById(R.id.RadioGroup);
+        rb1 = findViewById(R.id.rbOption1);
+        rb2 = findViewById(R.id.rbOption2);
+        rb3 = findViewById(R.id.rbOption3);
+        rb4 = findViewById(R.id.rbOption4);
+        btnConfirm = findViewById(R.id.btnConfirm);
+
+
+        textColorDefaultRb = rb1.getTextColors();
+
+
+
+        QuizDBHelper dbHelper = new QuizDBHelper(this);
+        questionList = dbHelper.getAllQuestions();
+        questionCountTotal = questionList.size();
+        Collections.shuffle(questionList);
+
+        showNextQuestion();
+
+
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!answered){
+                    if(rb1.isChecked() || rb2.isChecked() || rb3.isChecked() || rb4.isChecked()){
+                        checkAnswer();
+                    } else {
+                        Toast.makeText(Quiz.this,"Please select an answer",Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    showNextQuestion();
+                }
+            }
+        });
+
+
+    }
+
+    private void checkAnswer() {
+        answered = true;
+
+        RadioButton rbSelected = findViewById(rbGroup.getCheckedRadioButtonId());
+        int answer = rbGroup.indexOfChild(rbSelected) + 1;
+
+        if(answer == currentQuestion.getAnswer()){
+            score++;
+            tvScore.setText("Score: " +  score);
+        }
+
+        showSolution();
+    }
+
+    private void showSolution() {
+        rb1.setTextColor(Color.RED);
+        rb2.setTextColor(Color.RED);
+        rb3.setTextColor(Color.RED);
+        rb4.setTextColor(Color.RED);
+
+        switch (currentQuestion.getAnswer()){
+            case 1:
+                rb1.setTextColor(Color.GREEN);
+                tvQuestion.setText("Answer 1 is correct");
+                break;
+            case 2:
+                rb2.setTextColor(Color.GREEN);
+                tvQuestion.setText("Answer 2  is correct");
+                break;
+            case 3:
+                rb3.setTextColor(Color.GREEN);
+                tvQuestion.setText("Answer 3 is correct");
+                break;
+            case 4:
+                rb4.setTextColor(Color.GREEN);
+                tvQuestion.setText("Anser 4 is correct");
+                break;
+
+        }
+
+        if(questionCounter < questionCountTotal){
+            btnConfirm.setText("Next");
+
+        } else {
+            btnConfirm.setText("Finish");
+        }
+    }
+
+    private void showNextQuestion() {
+
+        rb1.setTextColor(textColorDefaultRb);
+        rb2.setTextColor(textColorDefaultRb);
+        rb3.setTextColor(textColorDefaultRb);
+        rb4.setTextColor(textColorDefaultRb);
+        rbGroup.clearCheck();
+
+        if(questionCounter < questionCountTotal) {
+            currentQuestion = questionList.get(questionCounter);
+
+            tvQuestion.setText(currentQuestion.getQuestion());
+            rb1.setText(currentQuestion.getOption1());
+            rb2.setText(currentQuestion.getOption2());
+            rb3.setText(currentQuestion.getOption3());
+            rb4.setText(currentQuestion.getOption4());
+
+            questionCounter++;
+            tvQuestionC.setText("Question: " +  questionCounter + "/" + questionCountTotal);
+            answered = false;
+            btnConfirm.setText("Confirm");
+
+
+        } else {
+            finishQuiz();
+        }
+    }
+
+    private void finishQuiz() {
+        finish();
+    }
+}
